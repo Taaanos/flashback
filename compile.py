6643bce03eea49973744b2d4bb74afbf2654ed37
Thanos Amoutzias <ktmfuji@gmail.com>
2023-08-26 21:54:40 +0200

    add debugging flag

--- compile.py
+++ compile.py
@@ -21,11 +21,15 @@ def compile_videos(folder, max_output_duration, min_clip_duration, max_clip_dura
     total_duration = 0
 
     for video_file in video_files:
+        print(f"Using video: {video_file}")
         if total_duration >= max_output_duration:
             break
 
         video_path = os.path.join(folder, video_file)
         video = VideoFileClip(video_path)
+
+        if args.debug:
+            video.save_frame(f"video_frame_{video_file}.png", t=video.duration/2)
 
         start_time = random.uniform(0, video.duration - min_clip_duration)
         end_time = random.uniform(start_time + min_clip_duration, min(start_time + max_clip_duration, video.duration))
@@ -39,7 +43,10 @@ def compile_videos(folder, max_output_duration, min_clip_duration, max_clip_dura
         total_duration += (end_time - start_time)
 
         video.reader.close()
-        print(f"Using video: {video_file}")
+
+    if args.debug:
+        for clip in clips:
+            clip.save_frame(f"clip_frame{video_file}.png", t=clip.duration/2)
 
     if clips:  # Check if there are valid clips
         final_video = concatenate_videoclips(clips)
@@ -65,6 +72,7 @@ if __name__ == "__main__":
     parser.add_argument('--max-output-duration', type=int, required=True, help='Maximum output duration.')
     parser.add_argument('--max-clip-duration', type=int, required=True, help='Maximum clip duration.')
     parser.add_argument('--min-clip-duration', type=int, required=True, help='Minimum clip duration.')
+    parser.add_argument('--debug', action='store_true', help='Enable debug mode.')
     args = parser.parse_args()
 
     compile_videos(args.folder, args.max_output_duration, args.min_clip_duration, args.max_clip_duration, args.order, args.extension)
